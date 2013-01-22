f28cf38aa9edb8339d01bc6c5befb37317ac7934
Matthew Hayes <matt@mysterycommand.com>
2013-01-21 19:11:07 -0600

    More tests. I don't want to have to set all the props in the constructor.

--- app/js/main.js
+++ app/js/main.js
@@ -21,23 +21,33 @@ define(
     ) {
         'use strict';
 
+        var obj = CoreObject.create();
+        console.dir(obj);
+        console.log(obj.name, obj.age);
+
+        
+
         var Child = CoreObject.extend({
-            name: '',
+            name: 'test',
             constructor: function(name) {
-                this.name = name;
+                // this.name = name;
             }
         });
         var child = Child.create('child');
-
-        var GrandChild = Child.extend();
-        var grandChild = GrandChild.create('grandChild');
-
-        console.dir(CoreObject.create());
         
         console.dir(child);
         console.log(child instanceof Child && child instanceof CoreObject);
+        console.log(child.name, child.age);
+
+        
+
+        var GrandChild = Child.extend({
+            age: 12
+        });
+        var grandChild = GrandChild.create('grandChild');
 
         console.dir(grandChild);
         console.log(grandChild instanceof GrandChild && grandChild instanceof Child && grandChild instanceof CoreObject);
+        console.log(grandChild.name, grandChild.age);
     }
 );
