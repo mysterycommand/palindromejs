9137d33f8ead04165ad7b62293d16958f423f3b7
Matt Hayes <matt@mysterycommand.com>
2013-06-15 19:40:50 -0500

    Clean up comments in CoreObject

--- app/scripts/library/core/CoreObject.js
+++ app/scripts/library/core/CoreObject.js
@@ -32,7 +32,9 @@ define([
      * functions.
      */
     var CoreObject = extend(Object, {
+
         // I like to put 'public static' members all the way at the top.
+
         /**
          * Proxies to the assignDescriptors method for convenience, particularly when inheriting values from a
          * base class's getter.
@@ -40,6 +42,7 @@ define([
          * @see     documentation in '/library/util/descriptors/assignDescriptors' for more.
          */
         assign: assignDescriptors,
+
         /**
          * Use CoreObject.create([instanceProps]) to generate instances. Currently it just proxies to new, but
          * might add functionality latyer. If instanceProps is passed, it's members will be copied onto the new
@@ -51,6 +54,7 @@ define([
         create: function(instanceProps) {
             return new this(instanceProps);
         },
+
         /**
          * Use CoreObject.extend([staticProps], [protoFn]) to create new "subclasses" (that is, constructors
          * with the appropriate prototype chain set up). Proxies out to `/library/core/extend`, but passes
@@ -65,12 +69,16 @@ define([
         extend: function(staticProps, protoFn) {
             return extend(this, staticProps, protoFn);
         },
+
         // I like to put 'private static' members after 'public static' members, but I'm not super clear
         // on what 'private static' even means in JavaScript ... maybe just non-configurable and non-enumerable
         // like with 'private' prototype methods.
+
     }, function(base) {
         return {
+
             // I like to put 'public' accessor members before the constructor.
+
             instanceDefaults: {
                 get: function() {
                     var ctor = this.constructor.name;
@@ -92,19 +100,22 @@ define([
                     };
                 }
             },
+
             /**
              * Creates a new instance of CoreObject. By default, anything returned from instanceDefaults will be
              * assigned to this. In objects that extend from CoreObject passing an object to instanceProps will
              * create members on the instance, but in the particular case of CoreObject _instanceId and _instanceName
-             * are non-configurable, and non-writable, so trying to override them will throw an error. They will
-             * be used for introspection later.
+             * are non-configurable, and non-writable, so trying to override them will throw an error. They are used
+             * for introspection (currently they just make instance.toString more meaningful).
              *
              * @param {Object} instanceProps An object who's properties will be copied onto the new instance.
              */
             constructor: function CoreObject(instanceProps) {
                 this.define(this.instanceDefaults, instanceProps || {});
             },
+
             // I like to declare 'public' data members after the constructor.
+
             /**
              * Use can for duck-typing your instances. Checks if 'key' is a function in the instance's prototype chain.
              *
@@ -114,8 +125,12 @@ define([
             can: function(key) {
                 return typeof this[key] === 'function';
             },
+
             /**
-             * Use has for duck-typing your instances. Checks if 'key' is an own property of this instance.
+             * Use has for duck-typing your instances. Checks if 'key' is an own property of this instance. Also, I just
+             * want to point out what an exercise in restraint it is for me not to name this method 'haz', and then chain
+             * it (along with can) and add another method 'cheezburger' so that `CoreObject.can.haz.cheezburger === true`.
+             * Just sayin'.
              *
              * @param  {String} key     The property name to look for.
              * @return {Boolean}        True if the propery is an own property of this instance.
@@ -123,6 +138,7 @@ define([
             has: function(key) {
                 return this.hasOwnProperty(key);
             },
+
             /**
              * Use describe to get own propery descriptors from an instance. Calling describe without arguments will
              * return an object with all property descriptors for this instance.
@@ -138,6 +154,7 @@ define([
                     ? Object.getOwnPropertyDescriptor(this, key)
                     : getDescriptors(this);
             },
+
             /**
              * Use define to define properties (via [provided or generated] property descriptor object) to instances.
              * @param  {...}     arguments  One or more objects to be converted to property descriptors and then defined on
@@ -149,6 +166,7 @@ define([
                 var descriptors = assignDescriptors.apply(null, arguments);
                 return Object.defineProperties(this, descriptors);
             },
+
             /**
              * Overrides the native Object.toString to give a more useful string representation of instances.
              *
@@ -157,6 +175,7 @@ define([
             toString: function() {
                 return '[' + this.instanceName + ' ' + this.constructor.name + ']';
             },
+
             /**
              * A public accessor for the automatically generated instance id.
              *
@@ -168,6 +187,7 @@ define([
                     return this._instanceId;
                 }
             },
+
             /**
              * A public accessor for the automatically generated instance name.
              *
@@ -179,6 +199,7 @@ define([
                     return this._instanceName;
                 }
             }
+
             // I like to put 'private' members at the bottom ... they're not really meant to be consumed, and anyway
             // in JavaScript 'private' essentially means non-configurable and non-enumerable ... you can still get
             // at them if you know their names.
